05a94aaba890c3c90ca8fd4fcc863ff71adf9b58
Maria Vitória <mavxsa@gmail.com>
2025-04-05 20:57:29 -0300

    aprendendo repetições

--- Operadores.java
+++ Operadores.java
@@ -2,13 +2,10 @@ public class Operadores {
 
     public static void main(String[] args) {
         int numero = 5;
+        //x repetição
+        numero++;
 
-        numero = - numero;
-
-            System.out.println(numero);
-
-        numero = numero * -1;
-
-            System.out.println(numero);
+        System.out.println(numero);
+       
     }
 }
