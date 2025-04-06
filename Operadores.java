5511959aa3c7093950a671b4857b8ea0f46dd570
Maria Vitória <mavxsa@gmail.com>
2025-04-05 23:20:13 -0300

    estudando operadores de condições ternárias.

--- Operadores.java
+++ Operadores.java
@@ -1,14 +1,26 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        int a, b;
 
-        a = 5;
-        b = 6;
+        String nomeUm = "Maria";
+        String nomeDois = new String("Maria");
 
-       String resultado = a==b ?"verdadeiro" : "false";
+        System.out.println(nomeUm.equals(nomeDois));
 
-        System.out.println(resultado);
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2) {
+            System.out.println("A nossa condição é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numero 2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois?" + simNao);
 
     }
 }
