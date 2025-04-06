3e77f511c8de37632a269b9f426d5240d055c44e
Maria Vitória <mavxsa@gmail.com>
2025-04-05 23:27:00 -0300

    Estudando operadores lógicos: && e ||.

--- Operadores.java
+++ Operadores.java
@@ -1,26 +1,22 @@
 public class Operadores {
 
     public static void main(String[] args) {
+            boolean condicao1=true;
 
-        String nomeUm = "Maria";
-        String nomeDois = new String("Maria");
+            boolean condicao2=false;
 
-        System.out.println(nomeUm.equals(nomeDois));
+            if(condicao1 && condicao2){
+                System.out.println("As duas condições são verdadeiras");
 
-        int numero1 = 1;
-        int numero2 = 2;
+            }
 
-        boolean simNao = numero1 == numero2;
+            if(condicao1 || condicao2){
+                System.out.println("Uma das condições é verdadeira");
 
-        if(numero1 < numero2) {
-            System.out.println("A nossa condição é verdadeira");
-        }
+            System.out.println("Fim!");
 
-        System.out.println("numeroUm é igual a numero 2? " + simNao);
+            }
 
-        simNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois?" + simNao);
 
     }
 }
