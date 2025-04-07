f60b8d485174e05167450e2025df64bedaebb14a
Maria Vitória <mavxsa@gmail.com>
2025-04-07 10:06:56 -0300

    tentativa de resolver o exercício: SmartTv

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,18 @@
 public class Metodos {
-     
+     public static String [] {
+        boolean ligada = true;
+        boolean desligada = false;
+
+            System.out.ln("Nossa tv está:" ligada)
+
+        int numero = 0;
+        int numero ++; 
+
+        System.out.println("Botao de volume:" numero);
+        
+        int canal = 0;
+        int canal ++;
+
+        System.out.println("Botam do canal:" canal);
+        }
 }
