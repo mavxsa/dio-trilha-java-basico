edaa17aa4a4a9ce90e3ca03d57ca23383fc554e8
Maria Vitória <mavxsa@gmail.com>
2025-04-13 11:20:36 -0300

    Fazendo a seção java doc - aprendendo sobre documentação e a fazer cometários. Isso ajuda a deixar o código claro e bem elaborado para nossos clientes e colaboradores.

---
+++ Comentarios.java
@@ -0,0 +1,44 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    
+
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você 
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+        public void metodo() {
+
+        }
+        /*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade somar ou  multiplicar
+ * dois números
+ */
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x
+    }
+    return r;
+}
+
+// Esse metodo acima não está cumprinco com as normas da linguagem (não é normas o nome exato)
+// Quero dizer a conduta do código
+// Mesmo com comentários ele deve ser bem elaborado e claro
+// Seguindo nossas anatomias de classes padroes.
+}
---
+++ PalavrasReservadas.mk
@@ -0,0 +1,1 @@
+fazer bonitinho depois:
