938399f2a893b8f9cf935a4eb062fc7aa28aa565
Maria Vitória <mavxsa@gmail.com>
2025-04-29 10:07:20 -0300

    imprimindo selecionados.

--- .vscode/controle-candidatos/src/Candidatura/ProcessoSeletivo2.java
+++ .vscode/controle-candidatos/src/Candidatura/ProcessoSeletivo2.java
@@ -2,8 +2,25 @@ package Candidatura;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo2 {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
         
+    }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice ++ ) {
+            System.out.println("O candidato de n" + (indice+1) + " é o " + candidatos [indice]);
+
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato: candidatos) { 
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
     static void selecaoCandidatos() {
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
