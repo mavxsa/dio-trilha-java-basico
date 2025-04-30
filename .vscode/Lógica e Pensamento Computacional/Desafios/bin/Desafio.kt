97b3f263f01f60cf77684228baf7542d64e51894
Maria Vitória <mavxsa@gmail.com>
2025-04-30 18:02:42 -0300

    desafio

--- .vscode/Lógica e Pensamento Computacional/Desafios/bin/Desafio.kt
+++ .vscode/Lógica e Pensamento Computacional/Desafios/bin/Desafio.kt
@@ -2,7 +2,8 @@ object ReceitaFederal {
     fun calcularImposto(salario: Double): Double {
         val aliquota = when {
             (salario >= 0 && salario <= 1100) -> 0.05
-            else -> TODO("Criar as condições para as aliquotas de 10.00% e 15.00%.")
+            (salario >= 1100.01 && salario <= 2500.00) -> 0.10
+            else -> 0.15 TODO("Criar as condições para as aliquotas de 10.00% e 15.00%.")
         }
         return aliquota * salario
     
